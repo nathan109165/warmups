9995143d7745a3f8ad2d322f6f7fe2316eacc4f5
nate999999 <160940728+nathan109165@users.noreply.github.com>
2025-11-21 10:05:26 -0500

    Update STARTER_Warm-Up_4_QC_Assignment.py

--- STARTER_Warm-Up_4_QC_Assignment.py
+++ STARTER_Warm-Up_4_QC_Assignment.py
@@ -1,1 +1,20 @@
+"""
+Filename: Warm-Up_4_QC_Hello.py
+Author: <Farlow, Nathan>
+Created: <11/21/2025>
+Instructor: Holtslander
+"""
 
+def hello():
+    """
+    Asks the user to input their name. Stores their name in a variable and prints the message:
+    "Hello, <name>"
+    :return: None
+    """
+    ### YOUR CODE GOES HERE ###
+    name = input('What is your name?\n')
+    print(f'Hello, {name}.')
+
+### YOU SHOULD NOT NEED TO CHANGE ANYTHING HERE ###
+if __name__ == '__main__':
+    hello()
